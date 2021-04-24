aed0462546425bf1787cb197f88ffb92361dd628
Davit Shirkhanyan <davit.shirkhanyan@gmail.com>
2021-04-23 21:46:44 -0700

    add record to the store

--- public/js/idb.js
+++ public/js/idb.js
@@ -6,8 +6,32 @@ const request = indexedDB.open('budget', 1);
 request.onupgradeneeded = event => {
     // save a reference to the database 
     const db = event.target.result;
-    db.createObjectStore('pending', {
+    db.createObjectStore('new_budget', {
         keyPath: 'id', 
         autoIncrement: true
     });
   };
+
+  request.onsuccess = function(event) {
+    // when db is successfully created with its object store (from onupgradedneeded event above), save reference to db in global variable
+    db = event.target.result;
+  
+    // check if app is online, if yes run checkDatabase() function to send all local db data to api
+    if (navigator.onLine) {
+      
+    }
+  };
+
+  request.onerror = function(event) {
+    // log error here
+    console.log(event.target.errorCode);
+  };
+
+  function saveRecord(record) {
+    const transaction = db.transaction('new_budget', 'readwrite');
+  
+    const budgetObjectStore = transaction.objectStore('new_budget');
+  
+    // add record to the store with add method.
+    budgetObjectStore.add(record);
+  }
